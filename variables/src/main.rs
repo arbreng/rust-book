e144aaaf40502d66b2842c0125b8de6b23aed981
David Worsham <arbreng@gmail.com>
2018-06-02 23:06:52 -0700

    Add variables project

---
+++ variables/src/main.rs
@@ -0,0 +1,71 @@
+fn main() {
+    // println!("___Immutability___");
+    // let _x = 5;
+    // println!("The value of x is: {}", x);
+    // x = 6;  // Not allowed, x is immutable by default.
+    // println!("The value of x is: {}", x);
+
+    println!("___Mutability___");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    println!("___Shadowing___");
+    let x =5 ;
+    let x = x + 1;
+    let x = x + 2;
+    println!("The value of x is: {}", x);
+
+    println!("___Types___");
+    let spaces = "   ";
+    let _spaces = spaces.len();
+
+    // let _mut spaces = "    ";
+    // spaces = spaces.len();  Not allowed, can't change types without let
+
+    // Not allowed, can't infer type in this case
+    //let _guess = "42".parse().expect("Not a number");
+    let _guess : u32 = "42".parse().expect("Not a number");
+
+    println!("___Floats___");
+    let _x = 2.0;
+    let _y: f32 = 3.0;
+
+    println!("___Numeric_Operations____");
+    let _sum = 5 + 10;
+    let _difference = 95.5 - 4.3;
+    let _product = 4 * 30;
+    let _quotient = 56.7 / 32.2;
+    let _remainder = 43 % 5;
+
+    println!("___Booleans___");
+    let _t = true;
+    let _f: bool = false;
+
+    println!("___Characters___");
+    let _c = 'z';
+    let _z = ' ';
+    let _heart_eyed_cat = '😻';
+
+    println!("___Tuples___");
+    let _tup: (i32, f64, u8) = (500, 6.4, 1);
+    let tup = (500, 6.4, 1);
+    let (_x, y, _z) = tup;
+    println!("The value of y is: {}", y);
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let _five_hundred = x.0;
+    let _six_point_four = x.1;
+    let _one = x.2;
+
+    println!("___Arrays___");
+    let a = [1, 2, 3, 4, 5];
+    let _months = ["January", "February", "March", "April", "May", "June",
+                  "July", "August", "September", "Octoboer", "November",
+                  "December"];
+    let _first = a[0];
+    let _second = a[1];
+    // let index = 10;
+    // let _element = a[index];  Not allowed, array out-of-bounds access
+    // /println!("The value of element is {}", element);
+}
